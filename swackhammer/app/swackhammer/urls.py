0c7be0b3f24ea8507fd30e021998fa7c782a2353
J-Spade <josh@jspade.net>
2024-07-13 17:33:36 -0400

    tweak swackhammer app

--- swackhammer/app/swackhammer/urls.py
+++ swackhammer/app/swackhammer/urls.py
@@ -6,10 +6,7 @@ from . import views
 
 
 urlpatterns = [
-    # main site
     path("", views.index, name="index"),
     path("signin/", views.signin, name="signin"),
     path("signout/", views.signout, name="signout"),
-    # admin
-    path("admin/", admin.site.urls),
 ] + staticfiles_urlpatterns()
